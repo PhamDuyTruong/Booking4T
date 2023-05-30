df80258f1e31a567da311cd73a9a27ba6e569d36
Duc Ho <hotienduc05012002@gmail.com>
2023-05-30 09:54:41 -0600

    Added read more button

--- blog-template/src/components/blog/BlogCard.js
+++ blog-template/src/components/blog/BlogCard.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import cardStyle from "./BlogCard.module.css";
+import Link from "next/link";
 
 const BlogCard = ({ index, title, thumbnail, description }) => {
   return (
@@ -13,7 +14,13 @@ const BlogCard = ({ index, title, thumbnail, description }) => {
       </div>
       <div className={cardStyle.textContainer}>
         <h1>{title}</h1>
-        <p>{description}</p>
+        <p>
+          {description}
+          {"... "}
+          <Link href={"#"} className={cardStyle.readMore}>
+            Read More
+          </Link>
+        </p>
       </div>
     </div>
   );
